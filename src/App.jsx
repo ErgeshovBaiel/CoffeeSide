e183a519003014dd8aac827f3017563c7f80382f
Baiel User <baiel@example.com>
2025-02-15 12:19:22 +0600

    add h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div> App
-
+    <div> 
+      <h1 className="text-3xl flex items-center justify-center   font-semibold text-green-700 ">
+        Hello World
+      </h1>
     </div>
   )
 }

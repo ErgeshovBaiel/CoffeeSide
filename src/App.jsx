1aad9333a5f36e47521244a5835e50dcc239ef4d
Baiel User <baiel@example.com>
2025-02-16 16:10:57 +0600

    Header with Hero done

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ import Hero from "./components/hero/Hero";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl flex items-center justify-center   font-semibold text-green-700 ">
-        Hello World
-      </h1>
       <Header />
       <Hero />
       <Card />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app-container flex gap-25">
+      <h1 className="w-[205px] h-[41px] mt-[48px] ml-[130px]
+       font-[Montserrat] font-extrabold text-[37.6px] leading-[41.16px]
+       bg-gradient-to-r from-[#35C66B] to-[#237249] bg-clip-text text-transparent text-6xl">StarBucks </h1>
+      <ul className="w-[400px] h-[23px] font-normal 
+      text-[20px] leading-[23.37px] flex gap-24
+      font-[Montserrat] mt-[57px] text-[#D9D9D9]  ">
+      <li>Home</li>
+      <li>Select</li>
+      <li>Shop</li>
+      </ul>
+      <div className='flex gap-5'>
+      <button></button>
+      <button></button>
+      </div>
+    </div>
   )
 }
 
